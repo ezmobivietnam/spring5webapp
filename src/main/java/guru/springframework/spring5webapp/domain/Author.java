143674dce3aef8acef14926486704edec7ffdcb3
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2020-07-12 10:21:06 -0400

    sync some diff code with branch display-authors-v2. Still cannot fix the bootup exception when register two controller the same time

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -29,6 +29,10 @@ public class Author {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
